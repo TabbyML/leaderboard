181a074ddab6930ea31e3e085bb904c9627564f0
Meng Zhang <meng@tabbyml.com>
2023-11-05 15:02:21 -0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
+import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from './components/theme-provider';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="light">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
 
