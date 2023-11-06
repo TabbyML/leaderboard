f8550371e1e67e298f03ea249d49926b4c3ff206
Meng Zhang <meng@tabbyml.com>
2023-11-05 16:22:45 -0800

    adapt mobile

--- src/App.tsx
+++ src/App.tsx
@@ -53,10 +53,9 @@ export default function App() {
   }, []);
 
   return (
-    <div className="w-screen flex flex-col items-center pt-20">
+    <div className="w-screen flex flex-col items-center pt-20 text-center">
       <p className="font-sf text-4xl">Coding LLMs Leaderboard</p>
-      <p className="mt-4 font-thin">Curated by <a target="_blank" className='underline decoration-slate-400' href="https://tabbyml.com">TabbyML team</a> with ❤️ in San Francisco</p>
-      <p className="mt-2 text-sm italic text-center mb-4">Last updated: 11/05/2023</p>
+      <p className="mt-4 font-thin">Curated by <a target="_blank" className='underline' href="https://tabby.tabbyml.com">Tabby Team</a> with ❤️ in San Francisco</p>
 
       <div className="mt-8">
         {false && <div className='flex justify-center mt-2 mb-6'>
@@ -72,14 +71,14 @@ export default function App() {
 
         {models.map(model => {
           return (
-            <div key={model.name} className="flex items-center text-sm metric-item">
-              <p className="font-semibold w-48 text-right mr-6 font-sf tracking-wide">{model.name}</p>
+            <div key={model.name} className="flex flex-col lg:flex-row text-sm metric-item items-start lg:items-center">
+              <p className="font-semibold font-sf tracking-wide lg:w-48 lg:mr-6 lg:text-right">{model.name}</p>
               <Metrics model={model} />
             </div>
           )
         })}
 
-        <div className='flex justify-center mt-14'>
+        <div className='flex justify-center my-14'>
           <MetricExplanation />
         </div>
       </div>
@@ -88,24 +87,25 @@ export default function App() {
 }
 
 function Metrics({ model }: { model: ModelItem }) {
+  const multiplier = 12;
   return <div>
     <div className='toggle-metric flex items-center gap-2'>
-      <div className="rounded-full h-2" style={{ width: `${model.baseline.average * 30}px`, background: 'linear-gradient(90deg, hsla(152, 100%, 60%, 0.5) 0%, hsla(186, 100%, 69%, 0.5) 100%)' }} />
+      <div className="rounded-full h-2" style={{ width: model.baseline.average * multiplier, background: 'linear-gradient(90deg, hsla(152, 100%, 60%, 0.5) 0%, hsla(186, 100%, 69%, 0.5) 100%)' }} />
       <span>{model.baseline.average}%</span>
     </div>
     <div className='flex items-center gap-2'>
-      <div className="rounded-full h-2" style={{ width: `${model.bm25.average * 30}px`, background: 'linear-gradient(90deg, hsla(279, 83%, 85%, 1) 0%, hsla(321, 90%, 70%, 1) 100%)' }} />
+      <div className="rounded-full h-2" style={{ width: model.bm25.average * multiplier, background: 'linear-gradient(90deg, hsla(279, 83%, 85%, 1) 0%, hsla(321, 90%, 70%, 1) 100%)' }} />
       <span>{model.bm25.average}%</span>
     </div>
     <div className='toggle-metric flex items-center gap-2'>
-      <div className="rounded-full h-2" style={{ width: `${model.oracle.average * 30}px`, background: 'linear-gradient(90deg, hsla(192, 95%, 70%, 0.6) 0%, hsla(225, 89%, 47%, 0.6) 100%)' }} />
+      <div className="rounded-full h-2" style={{ width: model.oracle.average * multiplier, background: 'linear-gradient(90deg, hsla(192, 95%, 70%, 0.6) 0%, hsla(225, 89%, 47%, 0.6) 100%)' }} />
       <span>{model.oracle.average}%</span>
     </div>
   </div>
 }
 
 function MetricExplanation() {
-  const width = 40;
+  const width = 20;
   return <div className='flex flex-col font-thin'>
     <div className='flex items-center gap-2'>
       <div className="rounded-full h-2" style={{ width, background: 'linear-gradient(90deg, hsla(152, 100%, 60%, 0.5) 0%, hsla(186, 100%, 69%, 0.5) 100%)' }} />
